52f0e8fa0c6ea3836455a23560bed152e7757965
ZuxinChen <116303234+ZuxinChen@users.noreply.github.com>
2023-01-30 18:15:46 -0500

    This is 1.0 version

--- src/main/java/com/mycompany/csc229_assignment01/NewClass.java
+++ src/main/java/com/mycompany/csc229_assignment01/NewClass.java
@@ -1,6 +1,8 @@
 
 package com.mycompany.csc229_assignment01;
 
+import java.util.Scanner;
+
 /**
  *
  * @author MoaathAlrajab
@@ -8,7 +10,29 @@ package com.mycompany.csc229_assignment01;
 public class NewClass {
     
      public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scnr = new Scanner(System.in); //able to input
+        Course crs = new Course(); // call the Couser class
+        int ID;
+        String name, code;
+        
+        //ask user to enter the ID, name and code
+        System.out.println("ID: ");
+        ID = scnr.nextInt();
+        System.out.println("name: ");
+        name = scnr.next();
+        System.out.println("code: ");
+        code = scnr.next();
+        
+        //assign these data in setters
+        crs.setID(ID);  
+        crs.setName(name);
+        crs.setCode(code);
+        
+        //print the ID, name and code by using the getters
+        System.out.println("ID: "+ crs.getID());
+        System.out.println("name: "+ crs.getName());
+        System.out.println("code: "+ crs.getCode());
+        
     }
     
 }
